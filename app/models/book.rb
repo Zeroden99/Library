80266f5c82267644d8d9c4f6b202ae1a7cf7ee2f
Den <ashenberg9@gmail.com>
2018-12-20 00:46:16 +0200

    Change password

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ApplicationRecord
 
 	mount_uploaders :libraries, LibraryUploader
   serialize :libraries, JSON # If you use SQLite, add this line.
+  validates :libraries, presence: true
 end
