9a8c73c1b7b43b862c448c7019845798649084bf
Den <ashenberg9@gmail.com>
2018-12-20 20:46:11 +0200

    Update

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,5 +4,6 @@ class Book < ApplicationRecord
 	mount_uploaders :libraries, LibraryUploader
   serialize :libraries, JSON # If you use SQLite, add this line.
   validates :name,:body,:libraries, presence: true
-  validates :name,:body, length: 8..256
+  validates :name, length: 4..256
+  validates :body, length: 8..256
 end
