bfb63c66a8c6b0fb9ad241ad0b692d8f5746b5ac
Den <ashenberg9@gmail.com>
2018-12-20 21:58:36 +0200

    gem pg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219222049) do
+ActiveRecord::Schema.define(version: 20181220192019) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20181219222049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "libraries"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "userparams", force: :cascade do |t|
